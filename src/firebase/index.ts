5962669058e05944f4a5103d2afcf70e0034d327
Julius V. Fadriquelan <jvfadriquelan@dswd.gov.ph>
2026-01-20 09:18:05 +0000

    just remove the offline

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,6 +1,6 @@
 import { initializeApp, getApp, getApps, type FirebaseApp } from 'firebase/app';
 import { getAuth, type Auth } from 'firebase/auth';
-import { getFirestore, type Firestore, enableIndexedDbPersistence } from 'firebase/firestore';
+import { getFirestore, type Firestore } from 'firebase/firestore';
 
 import { firebaseConfig } from './config';
 
@@ -18,19 +18,6 @@ export function initializeFirebase(): {
   
   if (!firestoreInstance) {
     const db = getFirestore(firebaseApp);
-    // Enable offline persistence
-    enableIndexedDbPersistence(db)
-      .catch((err) => {
-        if (err.code == 'failed-precondition') {
-          // Multiple tabs open, persistence can only be enabled
-          // in one tab at a a time.
-          console.warn('Firestore persistence failed: multiple tabs open.');
-        } else if (err.code == 'unimplemented') {
-          // The current browser does not support all of the
-          // features required to enable persistence
-          console.warn('Firestore persistence not available in this browser.');
-        }
-      });
     firestoreInstance = db;
   }
 
